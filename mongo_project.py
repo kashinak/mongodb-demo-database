d0e5499a53be4a9c5a52cc26adec04636208e3d6
kashinak <kashinakfx@gmail.com>
2021-09-11 03:08:59 +0000

    initial commit

---
+++ mongo_project.py
@@ -0,0 +1,22 @@
+import os
+import pymongo 
+if os.path.exists("env.py"):
+   import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI") 
+DATABASE = "myFirstDB"
+COLLECTION = "celebrities"
+
+
+def mongo_connect(url):
+    try: 
+        conn = pymongo.MongoClient(url)
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+def show_menu():
+    print("")
+    print("1. Add a record")
