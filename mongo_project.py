5d93da97fcf986f431d3b4cda013c0c6836a8898
kashinak <kashinakfx@gmail.com>
2021-09-12 22:55:49 +0000

    create add_record function in mongo_project.py

--- mongo_project.py
+++ mongo_project.py
@@ -20,3 +20,62 @@ def mongo_connect(url):
 def show_menu():
     print("")
     print("1. Add a record")
+    print("2. Find a record by name")
+    print("3. Edit a record")
+    print("4. Delete a record")
+    print("5. Exit")
+    
+    option = input("Enter option: ")
+    return option 
+
+
+def add_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    dob = input("Enter date of birth > ")
+    gender = input("Enter gender > ")
+    hair_color = input("Enter hair color > ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
+
+    new_doc = {
+        'first': first.lower(),
+        'last': last.lower(),
+        'dob': dob,
+        'gender': gender,
+        'hair_color': hair_color,
+        'occupation': occupation,
+        'nationality': nationality
+    }
+    
+    try: 
+        coll.insert(new_doc)
+        print('')
+        print('Document inserted')
+    except:
+        print('Error accessing the database')
+
+
+
+def main_loop():
+    while True:
+        option = show_menu()
+        if option == '1':
+            add_record()
+        elif option == '2':
+            print('You have selected option 2')
+        elif option == '3':
+            print('You have selected option 3')
+        elif option == '4':
+            print('You have selected option 4')
+        elif option == '5':
+            conn.close()
+            break
+        else:
+            print('Invalid option')
+        print("")
+
+conn = mongo_connect(MONGO_URI)
+coll = conn[DATABASE][COLLECTION]
+main_loop()
